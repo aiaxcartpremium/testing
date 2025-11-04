afa219a100f114c936d4bbc28c226bc18d5b5a08
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-05 03:11:46 +0800

    Create config.js

---
+++ aia-stock-test/config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://https://qddjhayaqkdcxqgdriav.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U";
