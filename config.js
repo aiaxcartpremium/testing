23719c2f12d52ff7054e3ed58c6ce5b07624e7f5
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-05 23:13:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
-<script>
-  // EDIT with your own values
-  window.SB_URL = "https://qddjhayaqkdcxqgdriav.supabase.co";
-  window.SB_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U";
 
-  // your fixed IDs
-  window.ID_OWNER = "8cd15b4b-0755-4843-a8d5-2652fa408fe5";
-  window.ID_ADMIN1 = "4e63c32b-cc75-48de-b111-e8a977d868a2";
-  window.ID_ADMIN2 = "20851a7b-ef92-41a1-80d1-d2a6081396d5";
-</script>
+/* ============
+   AiaxStock • config.js
+   Fill these with your real values.
+   ============ */
+
+// Supabase project
+window.SUPABASE_URL = 'https://qddjhayaqkdcxqgdriav.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U';
+
+// One (1) true owner UUID
+window.OWNER_UUID = '8cd15b4b-0755-4843-a8d5-2652fa408fe5';
+
+// Allowed admin UUIDs (add more if needed)
+window.ADMIN_UUIDS = [
+  '20851a7b-ef92-41a1-80d1-d2a6081396d5',
+  '4e63c32b-cc75-48de-b111-e8a977d868a2'
+];
