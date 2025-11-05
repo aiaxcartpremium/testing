6a789db2d4153907416ad4c8f4df4b4b19d07775
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-05 17:54:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
-<!-- config.js -->
-<script>
-  window.APP_CONFIG = {
-    supabaseUrl: "https://qddjhayaqkdcxqgdriav.supabase.co",
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U",
-    ownerId: "8cd15b4b-0755-4843-a8d5-2652fa408fe5",
-    adminIds: [
-      "4e63c32b-cc75-48de-b111-e8a977d868a2",
-      "20851a7b-ef92-41a1-80d1-d2a6081396d5"
-    ]
-  };
-</script>
+// config.js
+window.APP = {
+  url: 'https://qddjhayaqkdcxqgdriav.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U',
+  ownerId: '8cd15b4b-0755-4843-a8d5-2652fa408fe5',
+  admins: [
+    '4e63c32b-cc75-48de-b111-e8a977d868a2',
+    '20851a7b-ef92-41a1-80d1-d2a6081396d5'
+  ]
+};
