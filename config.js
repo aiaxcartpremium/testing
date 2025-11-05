5e09d54d4ae72a90d82310a6d1f66290f848cdd9
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-05 16:16:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-window.SUPABASE_URL = "https://https://qddjhayaqkdcxqgdriav.supabase.co";
-window.SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U";
-// Fill with your project values, then commit.
-// For magic link auth, set Supabase Auth -> URL Config -> Redirect URLs to your GitHub Pages domain.
+<!-- put this in /config.js -->
+<script>
+  window.SUPABASE_URL  = "https://qddjhayaqkdcxqgdriav.supabase.co"; // your project url
+  window.SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U";
+</script>
+<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
+<script>
+  window.supabase = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON);
+</script>
