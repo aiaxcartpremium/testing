df966383e49d0556aaea3f4f899ff13cd2c523eb
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-05 16:42:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-<!-- put this in /config.js -->
 <script>
-  window.SUPABASE_URL  = "https://qddjhayaqkdcxqgdriav.supabase.co"; // your project url
-  window.SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U";
-</script>
-<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
-<script>
-  window.supabase = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON);
+  // ======= EDIT THESE TWO LINES ONLY =======
+  const SUPABASE_URL = "https://qddjhayaqkdcxqgdriav.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U";
+  // ========================================
+
+  // Global Supabase client
+  window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 </script>
