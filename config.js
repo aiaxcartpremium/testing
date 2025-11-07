70e2301dffe0fc7eea20193ca17859e0d5817b1d
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-07 18:02:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 window.APP = {
   url: 'https://qddjhayaqkdcxqgdriav.supabase.co',
   key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZGpoYXlhcWtkY3hxZ2RyaWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyODEzMzUsImV4cCI6MjA3Nzg1NzMzNX0.Z_w3O9z6ZdZKt1TS3p4e6YYeUl1XHlCohLAatbA7g2U',
